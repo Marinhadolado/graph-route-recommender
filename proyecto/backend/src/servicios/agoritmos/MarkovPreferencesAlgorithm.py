d9e05c51ea194ac035e277ddfa75e6ed8ea697b6
madolatri <madolatri@gmail.com>
2026-05-17 19:08:10 +0200

    nuevo user_id 2

--- proyecto/backend/src/servicios/agoritmos/MarkovPreferencesAlgorithm.py
+++ proyecto/backend/src/servicios/agoritmos/MarkovPreferencesAlgorithm.py
@@ -87,6 +87,8 @@ class MarkovPreferencesAlgorithm(RecommendationAlgorithm):
                 print(f"[DEBUG NMF] ¡Usuario {user_id} ENCONTRADO en la matriz NMF! Fila: {user_matrix_idx}")
             else:
                 print(f"[DEBUG NMF] ¡ATENCIÓN! Usuario {user_id} NO EXISTE en NMF (Cold Start).")
+        for user_id, idx in self.modelo_nmf['user_map'].items():
+            print(f"User ID: {user_id} -> Matrix Index: {idx}")
 
         porcentaje_vecinos_temp = {}
         suma_nmf = 0.0
